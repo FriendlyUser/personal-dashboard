7c6c648416d22678c1e199332e707488bef083f4
FriendlyUser <pandabeareee@gmail.com>
2018-04-22 10:04:44 -0700

    adding new files, added grades boxplot

--- app.py
+++ app.py
@@ -13,25 +13,10 @@ app = dash.Dash(__name__)
 server = app.server
 
 # read data for tables (one df per table)
-df_fund_facts = pd.read_csv('https://plot.ly/~bdun9/2754.csv')
-df_price_perf = pd.read_csv('https://plot.ly/~bdun9/2756.csv')
-df_current_prices = pd.read_csv('https://plot.ly/~bdun9/2753.csv')
-df_hist_prices = pd.read_csv('https://plot.ly/~bdun9/2765.csv')
-df_avg_returns = pd.read_csv('https://plot.ly/~bdun9/2793.csv')
-df_after_tax = pd.read_csv('https://plot.ly/~bdun9/2794.csv')
-df_recent_returns = pd.read_csv('https://plot.ly/~bdun9/2795.csv')
-df_equity_char = pd.read_csv('https://plot.ly/~bdun9/2796.csv')
-df_equity_diver = pd.read_csv('https://plot.ly/~bdun9/2797.csv')
-df_expenses = pd.read_csv('https://plot.ly/~bdun9/2798.csv')
-df_minimums = pd.read_csv('https://plot.ly/~bdun9/2799.csv')
-df_dividend = pd.read_csv('https://plot.ly/~bdun9/2800.csv')
-df_realized = pd.read_csv('https://plot.ly/~bdun9/2801.csv')
-df_unrealized = pd.read_csv('https://plot.ly/~bdun9/2802.csv')
-
-df_graph = pd.read_csv("https://plot.ly/~bdun9/2804.csv")
-
 df_examS2018 = pd.read_csv("examSchedule.csv")
-# reusable componenets
+df_grades = pd.read_excel("gradesSpread.xlsx")
+
+# reusable components
 def make_dash_table(df):
     ''' Return a dash definition of an HTML table for a Pandas dataframe '''
     table = []
@@ -56,7 +41,7 @@ def get_logo():
         ], className="ten columns padded"),
 
         html.Div([
-            dcc.Link('Full View   ', href='/full-view')
+            dcc.Link('Full View   ', href='/full-view', className ="text-primary")
         ], className="two columns page-view no-print")
 
     ], className="row gs-header")
@@ -79,14 +64,14 @@ def get_menu():
     menu = html.Div([
 
         dcc.Link('Overview   ', href='/overview', className="tab first"),
-		dcc.Link('Co-op Job Hunt   ', href='/coop', className="tab"),
-		dcc.Link('Co-op Work Terms   ', href='/work-terms', className="tab"),
+        dcc.Link('Co-op Job Hunt   ', href='/coop', className="tab"),
+        dcc.Link('Co-op Work Terms   ', href='/work-terms', className="tab"),
         dcc.Link('Current Facts   ', href='/current-facts', className="tab"),
+        dcc.Link('Random Stuff   ', href='/random-stuff', className="tab"),
         dcc.Link('Academic TimeLine   ', href='/academic-timeline', className="tab")
 
     ], className="row ")
     return menu
-
 
 ## Page layouts
 overview = html.Div([  # page 1
@@ -579,33 +564,85 @@ coopJobStats = html.Div([  # page 3
         ], className="subpage")
     ], className="page")
 
-ganttData = [
-    dict(Task='Fall 1A', Start='2014-09-01', Finish='2014-12-28', Resource='firstYear'),
-    dict(Task='Spring 1B', Start='2015-01-01', Finish='2015-04-30', Resource='firstYear'),
-	dict(Task='Summer 1C', Start='2015-05-01', Finish='2015-08-31', Resource='firstYear'),
-	dict(Task='Fall 2A', Start='2015-09-01', Finish='2015-12-31', Resource='secondYear'),
-	dict(Task='Courses I', Start='2016-01-01', Finish='2016-04-30', Resource='Other'),
-	dict(Task='Summer 2B', Start='2016-05-01', Finish='2016-08-31', Resource='secondYear'),
-	dict(Task='ENGR-001', Start='2016-09-14', Finish='2016-12-31', Resource='Coop'),
-	dict(Task='ENGR-002', Start='2017-01-01', Finish='2017-04-29', Resource='Coop'),
-	dict(Task='Courses II', Start='2017-05-01', Finish='2017-09-30', Resource='Other'),
-	dict(Task='Fall 3B', Start='2017-09-01', Finish='2017-12-31', Resource='thirdYear'),
-	dict(Task='Spring 3B', Start='2018-01-01', Finish='2018-04-30', Resource='thirdYear'),
-	dict(Task='Job Hunt', Start='2015-05-01', Finish='2018-04-30', Resource='thirdYear')
-    # dict(Task='Anime', Start='2014-01-01', Finish='2019-01-01', Resource='Anime')
-]
-ganttData.reverse()
-colors = dict(firstYear = 'rgb(46, 17, 255)',
-			  secondYear = 'rgb(46, 255, 51)',
-			  thirdYear = 'rgb(255, 75, 25)',
-              Coop = 'rgb(114, 2, 121)',
-              Other = 'rgb(128, 17, 15)',
-			  Anime = 'rgb(18, 76, 165)')
-fig = ff.create_gantt(ganttData,colors,index_col='Resource', title='Academic Life Thus Far',
-                      show_colorbar=True, bar_width=0.2, showgrid_x=True, showgrid_y=True,
-					  height=575, width=725)
 
-currentFacts = html.Div([ # page 6
+## BoxPlot Data
+xls = pd.ExcelFile('gradesScatterPlot.xlsx')
+# First Year
+df1 = pd.read_excel(xls, 'term1A')
+df2 = pd.read_excel(xls, 'term1B')
+df3 = pd.read_excel(xls, 'term1C')
+
+# Second Year
+df4 = pd.read_excel(xls, 'term2A')
+df5 = pd.read_excel(xls, 'coursesI')
+df6 = pd.read_excel(xls, 'term2B')
+
+# First Year Courses
+year1AMyScores = df1['My Score'].tolist()
+year1AAverages = df1['Average'].tolist()
+year1BMyScores = df2['My Score'].tolist()
+year1BAverages = df2['Average'].tolist()
+year1CMyScores = df3['My Score'].tolist()
+year1CAverages = df3['Average'].tolist()
+
+firstYearScores = year1AMyScores + year1BMyScores + year1CMyScores
+firstYearAvgs   = year1AAverages + year1BAverages + year1CAverages
+
+firstYearLabel = ['Term 1A'] * len(year1AMyScores) +  \
+    ['Term 1B'] * len(year1BMyScores) + ['Term 1C'] * len(year1CMyScores)
+
+# Second Year Courses
+year2AMyScores = df4['My Score'].tolist()
+year2AAverages = df4['Average'].tolist()
+year2cIMyScores = df5['My Score'].tolist()
+year2cIAverages = df5['Average'].tolist()
+year2BMyScores = df6['My Score'].tolist()
+year2BAverages = df6['Average'].tolist()
+
+secondYearScores = year2AMyScores + year2cIMyScores + year2BMyScores 
+secondYearAvgs   = year2AAverages + year2cIAverages + year2BAverages
+
+secondYearLabel = ['Term 2A'] * len(year2AMyScores) +  \
+    ['Term 2.5'] * len(year2cIMyScores) + ['Term 2B'] * len(year2BMyScores)
+
+secondYearLabel = ['Term 2A'] * len(year2AMyScores) +  \
+    ['Term 2.5'] * len(year2cIMyScores) + ['Term 2B'] * len(year2BMyScores)
+
+# Plot configurations
+myScore = go.Box(
+    x=firstYearScores+secondYearScores,
+    y=firstYearLabel+secondYearLabel,
+    name='My Grade',
+    marker=dict(
+        color='#3D9970'
+    ),
+    orientation = 'h'
+)
+Average = go.Box(
+    x=firstYearAvgs+secondYearAvgs,
+    y=firstYearLabel+secondYearLabel,
+    name='Average',
+    marker=dict(
+        color='#FF4136'
+    ),
+     orientation = 'h'
+)
+
+# Set up Plot, and include under final exam tables
+data3 = [myScore, Average]
+layout3 = go.Layout(
+    yaxis=dict(
+        title='Academic Terms',
+        zeroline=False
+    ),
+    xaxis=dict(
+        title='Scores (out of 100)',
+        zeroline=False
+    ),
+    boxmode='group'
+)
+gradesBP = go.Figure(data=data3, layout=layout3)
+currentFacts = html.Div([ # page 4
  print_button(),
 
         html.Div([
@@ -623,12 +660,42 @@ currentFacts = html.Div([ # page 6
 				html.Div([
 				    html.H6('Spring 2018 Final Exam Schedule',
                             className="gs-header gs-text-header padded"),
-					html.Table(make_dash_table(df_examS2018))	
+					html.Table(make_dash_table(df_examS2018), className="table table-hover"),
+					#html.Table(make_dash_table(df_grades), className="table table-stripeed")					
 				], className="twelve columns"),
+				
+				# Row 2
+			    html.Div([
+                        html.H6('Grades',
+                                className="gs-header gs-text-header padded"),
+                        dcc.Graph(figure=gradesBP, id='boxplot'),
+                    ], className="twelve columns"),
+            ], className="row ")
+
+        ], className="subpage")
+
+    ], className="page")	
+
+
+randomStuff = html.Div([ # page 4
+ print_button(),
+
+        html.Div([
+
+            # Header
+
+            get_logo(),
+            get_header(),
+            html.Br([]),
+            get_menu(),
+
+            # Row 1
+
+            html.Div([	# Row 2
 				html.Div([
-					html.H6('Some Python',
-                            className="gs-header gs-text-header padded"),
-					dcc.SyntaxHighlighter('''
+						html.H6('Some Python',
+							className="gs-header gs-text-header padded"),
+						dcc.SyntaxHighlighter('''
 import python
 print(3)
 import easygui
@@ -647,13 +714,42 @@ for i in range(inputpdf.numPages):
         print("File: "+ "document-page%s.pdf" % i + " is printed")
 
 print("The script is done.")
-				''', language='python')
+				''', language='python'),
 				], className="twelve columns"),
             ], className="row ")
 
         ], className="subpage")
 
-    ], className="page")				
+    ], className="page")
+
+# Academic Timeline Data
+ganttData = [
+    dict(Task='Fall 1A', Start='2014-09-01', Finish='2014-12-28', Resource='firstYear'),
+    dict(Task='Spring 1B', Start='2015-01-01', Finish='2015-04-30', Resource='firstYear'),
+	dict(Task='Summer 1C', Start='2015-05-01', Finish='2015-08-31', Resource='firstYear'),
+	dict(Task='Fall 2A', Start='2015-09-01', Finish='2015-12-31', Resource='secondYear'),
+	dict(Task='Courses I', Start='2016-01-01', Finish='2016-04-30', Resource='Other'),
+	dict(Task='Summer 2B', Start='2016-05-01', Finish='2016-08-31', Resource='secondYear'),
+	dict(Task='ENGR-001', Start='2016-09-14', Finish='2016-12-31', Resource='Coop'),
+	dict(Task='ENGR-002', Start='2017-01-01', Finish='2017-04-29', Resource='Coop'),
+	dict(Task='Courses II', Start='2017-05-01', Finish='2017-09-30', Resource='Other'),
+	dict(Task='Fall 3B', Start='2017-09-01', Finish='2017-12-31', Resource='thirdYear'),
+	dict(Task='Spring 3B', Start='2018-01-01', Finish='2018-04-30', Resource='thirdYear'),
+	dict(Task='Job Hunt', Start='2015-05-01', Finish='2018-04-30', Resource='thirdYear'),
+	dict(Task='ENGR-003', Start='2018-05-01', Finish='2018-08-31', Resource='Coop')
+    # dict(Task='Anime', Start='2014-01-01', Finish='2019-01-01', Resource='Anime')
+]
+ganttData.reverse()
+colors = dict(firstYear = 'rgb(46, 17, 255)',
+			  secondYear = 'rgb(46, 255, 51)',
+			  thirdYear = 'rgb(255, 75, 25)',
+              Coop = 'rgb(114, 2, 121)',
+              Other = 'rgb(128, 17, 15)',
+			  Anime = 'rgb(18, 76, 165)')
+figGantt = ff.create_gantt(ganttData,colors,index_col='Resource', title='Academic Life Thus Far',
+                      show_colorbar=True, bar_width=0.2, showgrid_x=True, showgrid_y=True,
+					  height=575, width=725)
+					  
 academicTimeline = html.Div([  # page 6
 
         print_button(),
@@ -672,9 +768,9 @@ academicTimeline = html.Div([  # page 6
             html.Div([
 
                 html.Div([
-                    html.H6('Life Timeline',
+                    html.H6('Engineering Degree Timeline',
                             className="gs-header gs-text-header padded"),
-                    dcc.Graph(figure=fig, id='gantt'),
+                    dcc.Graph(figure=figGantt, id='gantt'),
                 ], className="twelve columns")
 
             ], className="row ")
@@ -709,10 +805,12 @@ def display_page(pathname):
         return coopJobStats 
     elif pathname == '/current-facts':
         return currentFacts
+    elif pathname == '/random-stuff':
+        return randomStuff
     elif pathname == '/academic-timeline':
         return academicTimeline
     elif pathname == '/full-view':
-        return overview,workTerm1and2,coopJobStats,academicTimeline
+        return overview,workTerms,coopJobStats,currentFacts,academicTimeline
     else:
         return noPage
 
